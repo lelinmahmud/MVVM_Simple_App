aff0e18ed6def64cdbd461ec4dfb3847f7ce4266
lelin <lelin.cse@gmail.com>
2019-12-01 00:06:11 +0600

    first commit

---
+++ app/src/main/java/com/lelin/mvvmsimpleapp/ui/auth/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.lelin.mvvmsimpleapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lelin.mvvmsimpleapp.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
